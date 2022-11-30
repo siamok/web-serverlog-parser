0d7f928bba4c0c377b37f80dc1386c1bfcf3c7db
Slawek Sz <szlosarczykslawomir@gmail.com>
2022-11-30 22:30:19 +0100

    unique and sorted result of visit aggregate

--- lib/parser/visit_aggregate.rb
+++ lib/parser/visit_aggregate.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 module Parser
   class VisitAggregate
@@ -11,6 +12,15 @@ module Parser
       @visits[page] = Parser::Visit.new unless visits.key?(page)
 
       @visits[page].add(ip)
+    end
+
+    def result(unique: false)
+      pages = @visits.each_with_object({}) do |visit, acc|
+        value = unique ? visit.last.ips.size : visit.last.count
+
+        acc[visit.first] = value
+      end
+      pages.sort_by { |_k, v| v }.reverse.to_h
     end
   end
 end
--- spec/lib/parser/visit_aggregate_spec.rb
+++ spec/lib/parser/visit_aggregate_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'parser/visit_aggregate'
 
 describe Parser::VisitAggregate do
-  let(:visit_aggregate) {Parser::VisitAggregate.new}
+  let(:visit_aggregate) { Parser::VisitAggregate.new }
   let(:page) { '/test/1' }
   let(:page1) { '/test/' }
   let(:ip) { '1.1.1.1' }
@@ -44,6 +46,25 @@ describe Parser::VisitAggregate do
       expect(visit_aggregate.visits[page]).to be_truthy
       expect(visit_aggregate.visits[page].count).to eq(2)
       expect(visit_aggregate.visits[page].ips).to eq([ip])
+    end
+  end
+
+  describe '#result' do
+    it 'returns result in hash form in order' do
+      visit_aggregate.add(page1, ip)
+      visit_aggregate.add(page, ip)
+      visit_aggregate.add(page, ip)
+
+      expect(visit_aggregate.result).to eq({ page => 2, page1 => 1 })
+    end
+
+    it 'returns unique result in hash form in order' do
+      visit_aggregate.add(page, ip)
+      visit_aggregate.add(page, ip)
+      visit_aggregate.add(page1, ip)
+      visit_aggregate.add(page1, ip1)
+
+      expect(visit_aggregate.result(unique: true)).to eq({ page1 => 2, page => 1 })
     end
   end
 end
